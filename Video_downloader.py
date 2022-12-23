92ae16bc54828d6a894899e5be9efb37510ab564
rodrigorotondo <rrotondo@fi.uba.ar>
2022-12-23 20:59:17 -0300

    cambio en la interfaz

--- Video_downloader.py
+++ Video_downloader.py
@@ -1,8 +1,10 @@
 from tkinter import filedialog,messagebox
+from tkinter import *
+import customtkinter
+import os
 import pytube
 from pytube.exceptions import *
-from tkinter import *
-import os
+import time
 
 
 save_path = os.getcwd()
@@ -11,6 +13,8 @@ save_path = os.getcwd()
 def descargar_video(link,nombre_archivo,calidad,extension):
     #descarga el video
     #obtiene el titulo del video
+
+    error = False
 
     EXTENSION_SIN_PUNTO = "mp4"
     nombre_archivo_salida = nombre_archivo.get()
@@ -42,82 +46,74 @@ def descargar_video(link,nombre_archivo,calidad,extension):
 
         except RegexMatchError:
             mensaje_al_usuario("La url es incorrecta!")
+            error = True
         except (VideoUnavailable, VideoPrivate , VideoRegionBlocked):
             mensaje_al_usuario("Video no disponible :(")
+            error = True
         except AttributeError:
             #este error se da cuando el objeto despues de ser filtrado no contiene la calidad elejida
             mensaje_al_usuario("El video no esta disponible en esa calidad.\nPor favor, elija otra.")
-        
-        #comprueba que el archivo exista
-        if archivo_existe(nombre_archivo_salida,extension):
+            error = True
+            
+        if(not error):
             mensaje_al_usuario("El archivo se descargo exitosamente!")
-        else:
-            mensaje_al_usuario("Algo fallo :(")
+        
     else:
         mensaje_al_usuario("Ya existe un archivo con ese nombre.")  
 
 
 def interfaz():
-    
-    raiz = Tk()
-    #dimensiones y colores
+    customtkinter.set_appearance_mode("dark")
+    customtkinter.set_default_color_theme("dark-blue")
+
+    raiz = customtkinter.CTk()
     raiz.title("Youtube Downloader")
-    raiz.iconbitmap("youtube.ico")
     raiz.geometry("560x350")
-    raiz.config(bg="#282828")
+    #raiz.config(bg="#282828")
     raiz.resizable(False,False)
 
-    #dimensiones y colores del frame
-    mi_frame= Frame(raiz, width="560", height="300")
-    mi_frame.config(bg="#282828")
+    mi_frame = customtkinter.CTkFrame(master = raiz)
     mi_frame.pack()
 
-    #label del titulo
-    label_inicial = Label(mi_frame, text="Youtube Downloader")
-    label_inicial.config(font=("Baskerville Old Face", 18), bg="#282828",fg="grey")
-    label_inicial.grid(padx=10, pady=10, row=0, column=0, columnspan=2)
-
     #label de la url
-    label_url = Label(mi_frame, text="URL",fg ="grey")
-    label_url.config(font=("Baskerville Old Face", 14), bg="#282828")
+    label_url = customtkinter.CTkLabel(master = mi_frame, text="URL")
+    label_url.configure(font=("Baskerville Old Face", 14))
     label_url.grid(padx=10, pady=10, row=1, column=0)
 
     #entry de la url
-    entry_url = Entry(mi_frame) 
-    entry_url.config(bg="black", width=35, insertbackground="whitesmoke",fg="grey",font=10)
+    entry_url = customtkinter.CTkEntry(master = mi_frame) 
     entry_url.grid(padx=10, pady=10, row=1, column=1, ipady=8)
 
     #label del nombre del archivo
-    label_nombre_del_archivo = Label(mi_frame, text="Nombre del archivo",fg="grey")
-    label_nombre_del_archivo.config(font=("Baskerville Old Face", 14), bg="#282828")
+    label_nombre_del_archivo = customtkinter.CTkLabel(master=mi_frame, text="Nombre del archivo")
+    label_nombre_del_archivo.configure(font=("Baskerville Old Face", 14))
     label_nombre_del_archivo.grid(padx=10, pady=10, row=2, column=0)
 
     #entry del nombre del archivo
-    entry_nombre_del_archivo = Entry(mi_frame)
-    entry_nombre_del_archivo.config(bg="black", width=35, insertbackground="whitesmoke", fg="grey",font=10)
+    entry_nombre_del_archivo = customtkinter.CTkEntry(master=mi_frame)
     entry_nombre_del_archivo.grid(padx=10, pady=10, row=2, column=1, ipady=8)
 
     #menu que muestra las calidades disponibles
-    variable_calidad = StringVar(raiz)
-    variable_calidad.set("Elija la calidad, por favor")
+    variable_calidad = customtkinter.StringVar(value="Elija la calidad, por favor")
     menu_opciones_calidad_lista = ["1080p" , "720p" , "480p" , "360p" , "240p" , "144p"]
-    menu_opciones_calidad = OptionMenu(raiz,variable_calidad,*menu_opciones_calidad_lista)
-    menu_opciones_calidad.config(bg="#282828",fg="gray")
-    menu_opciones_calidad.pack()
+    menu_opciones_calidad = customtkinter.CTkOptionMenu(raiz,values = menu_opciones_calidad_lista,variable=variable_calidad)
+    menu_opciones_calidad.pack(pady=10,padx=10)
+    menu_opciones_calidad.set("Elija la calidad, por favor")
 
     #boton para elegir la tura en la que se guarda el video
-    boton_elegir_ruta = Button(raiz,text="Elegir directorio de descarga" , command= lambda:elegir_directorio_de_descarga(raiz))
-    boton_elegir_ruta.config(width=34, font=("Baskerville Old Face",14) , bg="#282828",fg="grey")
-    boton_elegir_ruta.pack()
+    boton_elegir_ruta = customtkinter.CTkButton(master = raiz,text="Elegir directorio de descarga" , command= lambda:elegir_directorio_de_descarga(raiz))
+    boton_elegir_ruta.configure(width=34, font=("Baskerville Old Face",14))
+    boton_elegir_ruta.pack(pady=10,padx=10)
 
     #boton que llama a la funcion que descarga el video
-    boton_descargar_video = Button(raiz, text="¡Descargar Video!", command= lambda:descargar_video(entry_url,entry_nombre_del_archivo,variable_calidad,".mp4"))
-    boton_descargar_video.config(width=22, font=("Baskerville Old Face", 14), bg="#282828",fg="grey")
-    boton_descargar_video.pack()
+    boton_descargar_video = customtkinter.CTkButton(raiz, text="¡Descargar Video!", command= lambda:descargar_video(entry_url,entry_nombre_del_archivo,variable_calidad,".mp4"))
+    boton_descargar_video.configure(width=22, font=("Baskerville Old Face", 14))
+    boton_descargar_video.pack(padx=10,pady=10)
 
-    boton_descargar_audio = Button(raiz, text="¡Descargar Audio!", command= lambda:descargar_video(entry_url,entry_nombre_del_archivo,variable_calidad, ".mp3"))
-    boton_descargar_audio.config(width=22, font=("Baskerville Old Face", 14), bg="#282828",fg="grey")
-    boton_descargar_audio.pack()
+    #boton que llama a la funcion que descarga el audio
+    boton_descargar_audio = customtkinter.CTkButton(raiz, text="¡Descargar Audio!", command= lambda:descargar_video(entry_url,entry_nombre_del_archivo,variable_calidad, ".mp3"))
+    boton_descargar_audio.configure(width=22, font=("Baskerville Old Face", 14))
+    boton_descargar_audio.pack(padx=10,pady=10)
 
     raiz.mainloop()
 
@@ -125,15 +121,14 @@ def elegir_directorio_de_descarga(raiz):
     #esta funcion muestra una interfaz que permite ver el lugar donde se guarda el video para seleccionarla
     global save_path
     directorio_actual = os.getcwd()
-    directorio_elegido = filedialog.askdirectory(initialdir = directorio_actual, parent= raiz,title="Por favor, elija un directorio para su descarga")
+    directorio_elegido = customtkinter.filedialog.askdirectory(initialdir = directorio_actual, parent= raiz,title="Por favor, elija un directorio para su descarga")
 
     if directorio_elegido:
         save_path = directorio_elegido
     
 def mensaje_al_usuario(mensaje):
     #envia un mensaje al usuario en el que comunica algo pasado por parametro
-    
-    messagebox.showinfo('Atencion!', mensaje)
+    messagebox.showwarning('Atencion!', mensaje)
 
 def archivo_existe(nombre,extension):
     #comprueba la existencia de un video con el mismo nombre
@@ -143,5 +138,6 @@ def archivo_existe(nombre,extension):
         return False
 
 
-
 interfaz()
+    
+
